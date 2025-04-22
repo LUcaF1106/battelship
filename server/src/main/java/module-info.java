7100259342d38cc9b8d33d055d2abd148562d577
Roberto <104147441+Bousax@users.noreply.github.com>
2025-04-22 22:15:01 +0200

    add psw4j e GSON

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.itis._5a.frasson.busanello.server {
     requires com.itis._5a.frasson.busanello.common;
     requires static lombok;
+    requires com.google.gson;
+    requires password4j;
+
+    opens com.itis._5a.frasson.busanello.server to com.google.gson;
 
     exports com.itis._5a.frasson.busanello.server;
 }
